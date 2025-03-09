92587e570776d22b8e2b0fd795df5af6c33705f0
courtney08 <court1307@gmail.com>
2025-03-09 11:27:27 -0500

    Added Castle to the scene and RigidbodySleep Script

---
+++ Assets/__Scripts/RigidbodySleep.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(Rigidbody))]
+public class RigidbodySleep : MonoBehaviour
+{
+    private int sleepCountdown = 4;
+    private Rigidbody rigid;
+
+    void Awake(){
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate(){
+        if(sleepCountdown > 0){
+            rigid.Sleep();
+            sleepCountdown--;
+        }
+    }
+}
