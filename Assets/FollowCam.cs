f4d4602f1c5f089d25244d7fc1f50182f5a25d89
courtney08 <court1307@gmail.com>
2025-03-08 20:32:16 -0600

    Added FollowCam

--- Assets/FollowCam.cs
+++ Assets/FollowCam.cs
@@ -6,6 +6,10 @@ public class FollowCam : MonoBehaviour
 {
    static public GameObject POI;
 
+   [Header("Inscribed")]
+   public float easing = 0.05f;
+   public Vector2 minXY = Vector2.zero;
+
    [Header("Dynamic")]
    public float camZ;
 
@@ -14,10 +18,23 @@ public class FollowCam : MonoBehaviour
    }
 
    void FixedUpdate(){
-    if(POI == null) return;
+    Vector3 destination = Vector3.zero;
+    if(POI != null){
+        Rigidbody poiRigid = POI.GetComponent<Rigidbody>();
+        if((poiRigid != null) && poiRigid.IsSleeping()){
+            POI = null;
+        }
+    }
 
-    Vector3 destination = POI.transform.position;
+    if(POI != null){
+        destination = POI.transform.position;
+    }
+
+    destination.x = Mathf.Max(minXY.x, destination.x);
+    destination.y = Mathf.Max(minXY.y, destination.y);
+    destination = Vector3.Lerp(transform.position, destination, easing);
     destination.z = camZ;
     transform.position = destination;
+    Camera.main.orthographicSize = destination.y + 10;
    }
 }
