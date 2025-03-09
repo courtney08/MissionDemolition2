1a7568cb68d276456a40e10fb25024e6d4131316
courtney08 <court1307@gmail.com>
2025-03-09 12:40:15 -0500

    Added MissionDemolition Script

--- Assets/__Scripts/Projectile.cs
+++ Assets/__Scripts/Projectile.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 public class Projectile : MonoBehaviour
 {
     const int LOOKBACK_COUNT = 10;
+    static List<Projectile> PROJECTILES = new List<Projectile>();
 
     [SerializeField]
     private bool _awake = true;
@@ -26,6 +27,7 @@ public class Projectile : MonoBehaviour
         awake = true;
         prevPos = new Vector3(1000,1000,0);
         deltas.Add(1000);
+        PROJECTILES.Add(this);
     }
 
     // Update is called once per frame
@@ -50,6 +52,16 @@ public class Projectile : MonoBehaviour
         if(maxDelta <= Physics.sleepThreshold){
             awake = false;
             rigid.Sleep();
+        }
+    }
+
+    private void OnDestroy(){
+        PROJECTILES.Remove(this);
+    }
+
+    static public void DESTROY_PROJECTILES(){
+        foreach(Projectile p in PROJECTILES){
+            Destroy(p.gameObject);
         }
     }
 }
